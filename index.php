246cec7b7d17192a101b9429d5bcde9b2b8b2d25
Peggi Jelita <peggijelita@gmail.com>
2023-10-14 22:54:33 +0900

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum maxime debitis, natus nisi repellat libero saepe molestias ea laborum praesentium quae accusamus laudantium facilis iure in unde doloribus recusandae voluptas?</p>
+    <ul> 
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
